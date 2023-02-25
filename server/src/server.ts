1806f31340e3ab59aa1db7f93866e9ceb903e3f6
Daniel Carvalho <daniel.devop@gmail.com>
2023-02-25 16:30:58 -0300

    trazendo dados do DB

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
+import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
-app.get('/', ()=>{
-  return 'Hello World'
+app.get('/', async ()=>{
+  const habits = await prisma.habit.findMany()
+  return habits
 })
 
 app.listen({
