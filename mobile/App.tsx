45c5e433a41deba8fb46c66a40f67a379fd44674
Daniel Carvalho <daniel.devop@gmail.com>
2023-02-26 14:09:53 -0300

    import e seleção de fontes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold
+} from '@expo-google-fonts/inter';
 export default function App() {
+  useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
