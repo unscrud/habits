4acd79ed4f0cb943ea8db4e6a173d2f49c87530e
Daniel Carvalho <daniel.devop@gmail.com>
2023-02-26 15:01:16 -0300

    troca do staturbar do rn

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet,
+  Text, 
+  View, 
+  StatusBar 
+} from 'react-native';
+
 import { 
   useFonts,
   Inter_400Regular,
@@ -26,7 +31,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="light" />
+      <StatusBar barStyle="light-content" />
     </View>
   );
 }
