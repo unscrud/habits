032dae0c90547a9b4c791c17df9ab47847b56c0a
Daniel Carvalho <daniel.devop@gmail.com>
2023-02-26 14:50:25 -0300

    Exibe  loading caso fontes não carregadas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,13 +7,20 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold
 } from '@expo-google-fonts/inter';
+import { Loading } from './src/components/Loading';
 export default function App() {
-  useFonts({
+  const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
     Inter_800ExtraBold
   });
+
+  if(!fontsLoaded) {
+    return(
+      <Loading />
+    );
+  }
 
   return (
     <View style={styles.container}>
