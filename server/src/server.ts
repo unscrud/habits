d3632999fc626207c6c9b169eb3309a02980e941
Daniel Carvalho <daniel.devop@gmail.com>
2023-03-04 17:14:58 -0300

    removendo rotas e o client do prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,10 @@
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
-
+import { prisma } from "./lib/prisma";
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors, {
   origin: ['http://localhost:3000']
-})
-
-app.get('/', async ()=>{
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: 'Beber'
-      }
-    }
-  })
-  return habits
 })
 
 app.listen({
