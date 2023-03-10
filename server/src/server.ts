b1ab4e9372f9d8c6840195a30c1d93c4b8d9de7c
Daniel Carvalho <daniel.devop@gmail.com>
2023-03-10 20:17:31 -0300

    registrando as rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
 import cors from "@fastify/cors";
 import Fastify from "fastify";
-import { prisma } from "./lib/prisma";
+import { appRoutes } from "./routes";
 const app = Fastify()
 
 app.register(cors, {
   origin: ['http://localhost:3000']
 })
+app.register(appRoutes)
 
 app.listen({
   port: 3333
