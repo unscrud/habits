c0a3defa0d12b563b3781995550ba3e7567daa90
Daniel Carvalho <daniel.devop@gmail.com>
2023-03-19 18:16:36 -0300

    imagem do logo em estrutura básica

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
 import './styles/global.css'
 //import { Habit } from "./components/Habit"
 
+import logoImage from "./assets/logo.svg";
+
 export function App() {
   return (
-    <div>
-    
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <img src={logoImage} alt="Habits" />
+      </div>
     </div>
   )
 }
