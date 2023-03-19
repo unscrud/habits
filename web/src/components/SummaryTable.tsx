73c87a1f82f12d6820fcc6289aa2ecb5b890e82f
Daniel Carvalho <daniel.devop@gmail.com>
2023-03-19 19:39:53 -0300

    dias da semana no tabela de sumário

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,7 +1,19 @@
+const weekDays = ['D','S','T','Q','Q','S','S']
+
 export function SummaryTable(){
   return (
     <div className="w-full flex">
-      <div className="grid grid-rows-7"></div>
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {
+          weekDays.map((weekDay,i) => {
+            return (
+              <div key={`${weekDay}-${i}`} className="text-zinc-400 text-xl h-10 w-10 flex items-center justify-center">
+                {weekDay}
+              </div>
+            )
+          })
+        }       
+      </div>
     </div>
   )
 }
