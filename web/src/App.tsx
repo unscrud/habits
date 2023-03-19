9fc97e9805764256d49fabf2b054f2bc8969b736
Daniel Carvalho <daniel.devop@gmail.com>
2023-03-19 18:28:20 -0300

    estrutura superior com logo e button

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,7 +7,16 @@ export function App() {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
       <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
-        <img src={logoImage} alt="Habits" />
+        <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>  
+          <img src={logoImage} alt="Habits" />
+
+          <button 
+            type='button'
+            className='border border-violet-500 font-semibold rounded-lg px-6 py-4'
+          >
+            Novo Hábito
+          </button>
+        </div>
       </div>
     </div>
   )
