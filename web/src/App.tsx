13cec17214a04552bf8d53ae98814825685a7ac5
Daniel Carvalho <daniel.devop@gmail.com>
2023-03-19 19:14:15 -0300

    import do summaryTable

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import './styles/global.css'
 import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 //import { Habit } from "./components/Habit"
 
 export function App() {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
       <Header />
+      <SummaryTable />
     </div>
   )
 }
