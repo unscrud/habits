2099c8d1e47c2c1ebd48784ff8c388902854dac1
Daniel Carvalho <daniel.devop@gmail.com>
2023-03-25 15:11:59 -0300

    extraindo div do header para  app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,8 +6,10 @@ import { SummaryTable } from './components/SummaryTable'
 export function App() {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
-      <Header />
-      <SummaryTable />
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   )
 }
