72206eaaca29c56c60166b900d62696a6a7d002b
Daniel Carvalho <daniel.devop@gmail.com>
2023-04-01 23:04:49 -0300

    adição de dayhabits para placeholder com scroll

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,11 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
 import { generateDateFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 
 const weekDays = ['D','S','T','Q','Q','S','S']
 const datesFromYearStart = generateDateFromYearBeginning()
-
+const minimumSummaryDateSizes = 18 * 7
+const amountOfDaysToFill = minimumSummaryDateSizes - datesFromYearStart.length
 
 export function Home(){
   return (
@@ -28,16 +29,30 @@ export function Home(){
         }
       </View>
 
-      <View className="flex-row flex-wrap">
-        {
-          datesFromYearStart.map(date => (
-            <HabitDay
-              key={date.toISOString()}
-            />
-          ))
-        }
-      </View>
-      <HabitDay />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 100}}
+      >
+        <View className="flex-row flex-wrap">
+          {
+            datesFromYearStart.map(date => (
+              <HabitDay
+                key={date.toISOString()}
+              />
+            ))
+          }
+          {
+            amountOfDaysToFill > 0 && Array
+              .from({length: amountOfDaysToFill})
+              .map((_,index) =>(
+                <View
+                  className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                  style={{width: DAY_SIZE, height: DAY_SIZE}}
+                />
+              ))
+          }
+        </View>
+      </ScrollView>
     </View>
   )
 }
