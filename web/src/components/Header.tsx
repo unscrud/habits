73ea5bd2e8c0d5577b8450dd5eb9a771713aa722
Daniel Carvalho <daniel.devop@gmail.com>
2023-04-07 17:56:13 -0300

    usando o radix dialog para fazer o modal

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,28 +1,29 @@
 import { Plus } from 'phosphor-react'
-import { useState } from 'react';
+import * as Dialog from "@radix-ui/react-dialog";
 import logoImage from "../assets/logo.svg";
 
 export function Header() {
-  let [isModalOpen, setIsModalOpen] = useState(false)  
-
-  function buttonClicked(){
-    setIsModalOpen(true)
-  }
-
   return (
     <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>  
       <img src={logoImage} alt="Habits" />
 
-      { isModalOpen ? <p>oi</p> : null }
+      <Dialog.Root>
+        <Dialog.Trigger
+          type='button'
+          className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
+        >
+          <Plus size={20} className='text-violet-500' />
+          Novo Hábito
+        </Dialog.Trigger>
 
-      <button 
-        type='button'
-        className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
-        onClick={buttonClicked}
-      >
-        <Plus size={20} className='text-violet-500' />
-        Novo Hábito
-      </button>
+        <Dialog.Portal>
+          <Dialog.Overlay 
+            className='w-screen h-screen bg-black/80 fixed inset-0'
+          />
+          <Dialog.Content>
+            Conteúdo do Modal
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
-  )
-}
+  )}
