60c968873e3b1e8199fed958b433c4542c671146
Daniel Carvalho <daniel.devop@gmail.com>
2023-04-08 07:47:31 -0300

    btn de fechar, título e chamada do form

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,6 +1,7 @@
-import { Plus } from 'phosphor-react'
+import { Plus, X } from 'phosphor-react'
 import * as Dialog from "@radix-ui/react-dialog";
 import logoImage from "../assets/logo.svg";
+import { NewHabitForm } from './NewHabitForm';
 
 export function Header() {
   return (
@@ -22,7 +23,13 @@ export function Header() {
           />
           <Dialog.Content className='absolute p-10 bg-zinc-900 rounded-2xl
            w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-            Conteúdo do Modal
+            <Dialog.Close className='absolute right-6 top-6 text-zinc-400 hover:text-zinc-200'>
+              <X size={24} aria-label='Fechar' />
+            </Dialog.Close>
+            <Dialog.Title className='text-3xl leading-tight font-extrabold'>
+              Criar hábito
+            </Dialog.Title>
+            <NewHabitForm />
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
