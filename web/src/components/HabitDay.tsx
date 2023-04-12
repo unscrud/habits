e736b0fe613693b0e0673be0c2c853513943cc4a
Daniel Carvalho <daniel.devop@gmail.com>
2023-04-11 21:44:45 -0300

    esboço do popover

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,11 +1,26 @@
+import * as Popover from '@radix-ui/react-popover'
+
 // interface HabitDayProps {
 //   completed: number
 // }
 
 export function HabitDay(/*props: HabitDayProps*/) {
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg">
-      {/*props.completed*/}
-    </div>
+    <Popover.Root>
+      <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg">
+        <Popover.Portal>
+          <Popover.Content className='min-w-[323px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
+            <span className='font-semibold text-zinc-400'>Terça-Feira</span>
+            <span className='mt-1 font-extrabold leading-tight text-3xl'>17/01</span>
+
+            <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
+              <div className='h-3 rounded-xl bg-violet-600 w-3/4' />
+            </div>
+
+            <Popover.Arrow height={8} width={16} className='fill-zinc-900'/>
+          </Popover.Content>
+        </Popover.Portal>
+      </Popover.Trigger>
+    </Popover.Root>
   )
 }
