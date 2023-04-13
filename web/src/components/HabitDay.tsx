97a643d52aa6b13a17c5d88e5e7e6044d1a99532
Daniel Carvalho <daniel.devop@gmail.com>
2023-04-13 20:28:14 -0300

    movendo a progressbar para um componente

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,5 +1,5 @@
 import * as Popover from '@radix-ui/react-popover'
-
+import { ProgressBar } from './ProgressBar'
 // interface HabitDayProps {
 //   completed: number
 // }
@@ -13,18 +13,7 @@ export function HabitDay(/*props: HabitDayProps*/) {
             <span className='font-semibold text-zinc-400'>Terça-Feira</span>
             <span className='mt-1 font-extrabold leading-tight text-3xl'>17/01</span>
 
-            <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
-              
-              <div
-                role='progressbar'
-                aria-label='Progresso de hábitos completados'
-                aria-valuenow={75}
-                aria-aria-valuemin={0}
-                aria-aria-valuemax={100}
-                className='h-3 rounded-xl bg-violet-600 w-3/4' 
-              />
-
-            </div>
+            <ProgressBar />
 
             <Popover.Arrow height={8} width={16} className='fill-zinc-900'/>
           </Popover.Content>
---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,14 @@
+export function ProgressBar(){
+  return (
+    <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>          
+      <div
+        role='progressbar'
+        aria-label='Progresso de hábitos completados'
+        aria-valuenow={75}
+        aria-aria-valuemin={0}
+        aria-aria-valuemax={100}
+        className='h-3 rounded-xl bg-violet-600 w-3/4' 
+      />
+    </div>
+  )
+}
