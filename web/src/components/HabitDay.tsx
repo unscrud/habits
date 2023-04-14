09e9ec59018504bc17be03a52c384e8848e8ac2f
Daniel Carvalho <daniel.devop@gmail.com>
2023-04-13 22:02:46 -0300

    classname condicional

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -18,7 +18,7 @@ export function HabitDay({completed, amount}: HabitDayProps) {
           'bg-violet-900 border-violet-800': completedPercentage > 0 && completedPercentage < 20,
           'bg-violet-800 border-violet-700': completedPercentage >= 20 && completedPercentage < 40,
           'bg-violet-700 border-violet-600': completedPercentage >= 40 && completedPercentage < 60,
-          'bg-violet-600 border-violet-500': completedPercentage >= 80 && completedPercentage < 80,
+          'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
           'bg-violet-500 border-violet-400': completedPercentage >= 80
         })}
       >
