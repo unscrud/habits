221d1bdcc75163ebbacd1b9f34e1ae67a7a273fc
Daniel Carvalho <daniel.devop@gmail.com>
2023-04-16 19:54:28 -0300

    recebendo a data

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,11 +1,16 @@
 import { ScrollView, View, Text } from "react-native";
 import { BackButton } from "../components/BackButton";
+import { useRoute } from "@react-navigation/native";
 
 interface Params {
   date: string
 }
 
 export function Habit() {
+  const route = useRoute()
+  const {date} = route.params as Params
+
+  console.log(date)
   return (
     <View 
       className="flex-1 bg-background px-8 pt-16"
