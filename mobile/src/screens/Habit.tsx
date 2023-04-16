7ecee13b01639bdf6fc185efa2537158d7614c7b
Daniel Carvalho <daniel.devop@gmail.com>
2023-04-16 19:26:27 -0300

    Tela sem scroll visual com espaço de 100 no final

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,9 +1,18 @@
-import { View } from "react-native";
+import { ScrollView, View, Text } from "react-native";
+import { BackButton } from "../components/BackButton";
+
 
 export function Habit() {
   return (
     <View 
       className="flex-1 bg-background px-8 pt-16"
-    ></View>
+    >
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 100}}
+      >
+        
+      </ScrollView>
+    </View>
   )
 }
