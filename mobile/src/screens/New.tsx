73f0224d1c3bfd7b1c81e4002d607ba260867d86
Daniel Carvalho <daniel.devop@gmail.com>
2023-04-16 06:28:47 -0300

    titulo da tela e imput com label

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, View } from "react-native";
+import { ScrollView, View, Text, TextInput } from "react-native";
 import { BackButton } from "../components/BackButton";
 
 export function New() {
@@ -8,6 +8,22 @@ export function New() {
     >
       <ScrollView showsVerticalScrollIndicator={false}>
         <BackButton />
+
+        <Text 
+          className="mt-6 text-white font-extrabold text-3xl"
+        >
+          Criar Hábito
+        </Text>
+
+        <Text 
+          className="mt-6 text-white font-semibold text-3xbase"
+        >
+          Qual seu comprometimento
+        </Text>
+
+        <TextInput
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600" 
+        />
       </ScrollView>
     </View>
   )
