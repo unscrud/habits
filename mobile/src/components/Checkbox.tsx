85c239629da8925dcf15e5929e03ecbbe9fc19db
Daniel Carvalho <daniel.devop@gmail.com>
2023-04-16 12:57:30 -0300

    check marcado ou não e com title

--- mobile/src/components/Checkbox.tsx
+++ mobile/src/components/Checkbox.tsx
@@ -1,21 +1,34 @@
-import { TouchableOpacity, View } from "react-native";
+import { TouchableOpacity, View, Text } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
 
-export function Checkbox(){
+interface Props {
+  title: string
+  checked?: boolean
+}
+
+export function Checkbox( { title, checked = false  }: Props){
   return (
     <TouchableOpacity
       activeOpacity={0.7}
       className="flex-row mb-2 items-center"
     >
-      <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
-        <Feather 
-          name="check"
-          size={20}
-          color={colors.white}
-        />
-      </View>
-      
+      { 
+        checked ? 
+          <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
+            <Feather 
+              name="check"
+              size={20}
+              color={colors.white}
+            />
+          </View>
+        : 
+          <View className="h-8 w-8 bg-zinc-900 rounded-lg"/>
+      }
+
+      <Text className="text-white ml-3">
+        { title }
+      </Text>
     </TouchableOpacity>
   )
 }
