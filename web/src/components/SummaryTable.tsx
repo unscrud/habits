a412d82edd2e4669943d4803550669005ab08f25
Daniel Carvalho <daniel.devop@gmail.com>
2023-04-21 13:48:07 -0300

    buscando no carregamento o summary da api

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -18,6 +18,14 @@ type Summary = {
 }[]
 
 export function SummaryTable(){
+  const [summary, setSummary] = useState<Summary>([])
+
+  useEffect(()=>{
+    api.get('summary').then(response => {
+      setSummary(response.data)
+    })
+  },[])
+
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
