5b8e0ca010a78439f4701b41c8c8fcb544f0876f
Daniel Carvalho <daniel.devop@gmail.com>
2023-05-06 13:28:39 -0300

    garantindo que a comparação é com o final do dia

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -28,7 +28,7 @@ export function HabitsList( { date } : HabitsListProps ){
     }).then(response => setHabitsInfo(response.data))
   },[])
 
-  const isDateInPast = dayjs(date).isBefore(new Date())
+  const isDateInPast = dayjs(date).endOf('day').isBefore(new Date())
  
   return (
     <div className='mt-6 flex flex-col gap-3'>
