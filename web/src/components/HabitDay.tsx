c172f7982e66c9ff2277b02b2742b477991ca4df
Daniel Carvalho <daniel.devop@gmail.com>
2023-05-07 19:01:03 -0300

    criação de estado e de valor padrão para completed

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -3,21 +3,24 @@ import { ProgressBar } from './ProgressBar'
 import clsx from 'clsx'
 import dayjs from 'dayjs'
 import { HabitsList } from './HabitsList'
+import { useState } from 'react'
 
 interface HabitDayProps {
   date: Date
-  completed?: number
+  defaultCompleted?: number
   amount?: number
 }
 
-export function HabitDay({date, completed=0, amount=0}: HabitDayProps) {
+export function HabitDay({date, defaultCompleted=0, amount=0}: HabitDayProps) {
+  const [completed, setCompleted] = useState(defaultCompleted)
+
   const  completedPercentage = amount > 0 ? Math.round( (completed / amount) * 100 ) : 0
 
   const dayAndMonth = dayjs(date).format('DD/MM')
   const dayOfWeek = dayjs(date).format('dddd')
 
   function handleCompletedChanged(completed: number){
-    console.log(completed)
+    setCompleted(completed)
   }
 
   return (
