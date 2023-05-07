00e1324d33132070618d9b22059f40d37c403996
Daniel Carvalho <daniel.devop@gmail.com>
2023-05-07 17:41:10 -0300

    toggle function ao marcar ou desmarcar um hábito

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -28,6 +28,25 @@ export function HabitsList( { date } : HabitsListProps ){
     }).then(response => setHabitsInfo(response.data))
   },[])
 
+  async function handleToggleHabit(habitId:string) {
+    await api.patch(`/habits/${habitId}/toggle`)
+    
+    const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId)
+
+    let completedHabits: string[] = []
+
+    if (isHabitAlreadyCompleted) {
+      completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId)
+    } else {
+      completedHabits = [...habitsInfo!.completedHabits, habitId]
+    }
+ 
+    setHabitsInfo({
+      possibleHabits: habitsInfo!.possibleHabits,
+      completedHabits
+    })
+  }
+
   const isDateInPast = dayjs(date)
                         .endOf('day')
                         .isBefore(new Date())
@@ -39,6 +58,7 @@ export function HabitsList( { date } : HabitsListProps ){
           return (
             <Checkbox.Root 
               key={habit.id}
+              onCheckedChange={ () => handleToggleHabit(habit.id)}
               checked={habitsInfo.completedHabits.includes(habit.id)}
               disabled={isDateInPast}
               className='flex items-center gap-3 group'
