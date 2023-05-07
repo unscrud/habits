3aec8387f191c0e76cf6ad8aae2c2e5558f9e6e2
Daniel Carvalho <daniel.devop@gmail.com>
2023-05-07 18:48:42 -0300

    adição e atualização de um novo parâmetro

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -6,6 +6,7 @@ import dayjs from 'dayjs';
 
 interface HabitsListProps{
   date: Date
+  onCompletedChanged: (completed: number) => void
 }
 
 interface HabitsInfo {
@@ -17,7 +18,7 @@ interface HabitsInfo {
    completedHabits: string[]
 }
 
-export function HabitsList( { date } : HabitsListProps ){ 
+export function HabitsList( { date, onCompletedChanged } : HabitsListProps ){ 
   const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>()
 
   useEffect(()=>{
@@ -40,11 +41,13 @@ export function HabitsList( { date } : HabitsListProps ){
     } else {
       completedHabits = [...habitsInfo!.completedHabits, habitId]
     }
- 
+
     setHabitsInfo({
       possibleHabits: habitsInfo!.possibleHabits,
       completedHabits
     })
+
+    onCompletedChanged(completedHabits.length)
   }
 
   const isDateInPast = dayjs(date)
