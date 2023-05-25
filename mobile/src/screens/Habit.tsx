643956ce77b986970a089977f0b5b70e84bccf8f
Daniel Carvalho <daniel.devop@gmail.com>
2023-05-24 22:14:06 -0300

    formatação do eslint

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,9 +1,9 @@
-import { ScrollView, View, Text } from "react-native";
-import { BackButton } from "../components/BackButton";
 import { useRoute } from "@react-navigation/native";
 import dayjs from "dayjs";
-import { ProgressBar } from "../components/ProgressBar";
+import { ScrollView, Text, View } from "react-native";
+import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
+import { ProgressBar } from "../components/ProgressBar";
 
 interface Params {
   date: string
@@ -11,28 +11,28 @@ interface Params {
 
 export function Habit() {
   const route = useRoute()
-  const {date} = route.params as Params
+  const { date } = route.params as Params
 
   const parsedDate = dayjs(date)
   const dayOfWeek = parsedDate.format('dddd')
   const dayAndMonth = parsedDate.format('DD/MM')
 
   return (
-    <View 
+    <View
       className="flex-1 bg-background px-8 pt-16"
     >
       <ScrollView
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={{paddingBottom: 100}}
+        contentContainerStyle={{ paddingBottom: 100 }}
       >
-        <BackButton/>
+        <BackButton />
 
         <Text
           className="text-zinc-400 mt-6 font-semibold text-base lowercase"
         >
           {dayOfWeek}
         </Text>
-        
+
         <Text
           className="text-white font-extrabold text-3xl"
         >
@@ -50,7 +50,7 @@ export function Habit() {
             checked={true}
           />
         </View>
-        
+
       </ScrollView>
     </View>
   )
