4da7bb75310b87c27148c658147ae5eb81b220a8
Daniel Carvalho <daniel.devop@gmail.com>
2023-05-24 21:46:02 -0300

    adição da url no status de execução do servidor

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,6 @@ app.register(appRoutes)
 app.listen({
   port: 3333,
   host: '0.0.0.0'
-}).then(()=>{
-  console.log('HTTP Server running!')
+}).then((url) => {
+  console.log(`HTTP Server running on ${url}`)
 })
