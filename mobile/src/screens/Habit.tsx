f2d45230ced94e6291f48e1934647948615cb86d
Daniel Carvalho <daniel.devop@gmail.com>
2023-06-03 12:12:24 -0300

    progress bar atualizado de acordo com as marcações

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -7,6 +7,7 @@ import { Checkbox } from "../components/Checkbox";
 import { Loading } from "../components/Loading";
 import { ProgressBar } from "../components/ProgressBar";
 import { api } from "../lib/axios";
+import { generateProgressPercentage } from "../utils/generate-progress-percentage";
 
 interface Params {
   date: string
@@ -31,6 +32,10 @@ export function Habit() {
   const parsedDate = dayjs(date)
   const dayOfWeek = parsedDate.format('dddd')
   const dayAndMonth = parsedDate.format('DD/MM')
+
+  const habitsProgress = dayInfo?.possibleHabits.length
+    ? generateProgressPercentage(dayInfo.possibleHabits.length, completedHabits.length)
+    : 0
 
   async function fetchHabits() {
     try {
@@ -88,7 +93,7 @@ export function Habit() {
           {dayAndMonth}
         </Text>
 
-        <ProgressBar progress={75} />
+        <ProgressBar progress={habitsProgress} />
         <View className="mt-6">
           {
             dayInfo?.possibleHabits &&
