7b37b9ef1b7f35f42342724ba771880dd83e2afa
Daniel Carvalho <daniel.devop@gmail.com>
2023-06-05 21:03:20 -0300

    auto formatação do eslint

--- mobile/src/components/Checkbox.tsx
+++ mobile/src/components/Checkbox.tsx
@@ -1,5 +1,5 @@
-import { TouchableOpacity, TouchableOpacityProps, View, Text } from "react-native";
 import { Feather } from "@expo/vector-icons";
+import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
 import colors from "tailwindcss/colors";
 
 interface Props extends TouchableOpacityProps {
@@ -7,30 +7,30 @@ interface Props extends TouchableOpacityProps {
   checked?: boolean
 }
 
-export function Checkbox( { title, checked = false, ...rest  }: Props){
+export function Checkbox({ title, checked = false, ...rest }: Props) {
   return (
     <TouchableOpacity
       activeOpacity={0.7}
       className="flex-row mb-2 items-center"
       {...rest}
     >
-      { 
-        checked ? 
+      {
+        checked ?
           <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
-            <Feather 
+            <Feather
               name="check"
               size={20}
               color={colors.white}
             />
           </View>
-        : 
-          <View className="h-8 w-8 bg-zinc-900 rounded-lg"/>
+          :
+          <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
       }
 
-      <Text 
+      <Text
         className="text-white ml-3 font-semibold"
       >
-        { title }
+        {title}
       </Text>
     </TouchableOpacity>
   )
